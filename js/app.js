cc969d6b7ad405d25a66de9047cc47f22eb348dd
moein <99531013+s-moein-p@users.noreply.github.com>
2023-11-19 11:12:20 +0330

    book-store

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// const products = [
+//     {
+//         id :1,
+//         name: 'book-1',
+//         price: 99,
+//         image: '../img/11-111.png',
+//     }
+// ]
+
+// const  renderproducts = ( ) => {
+//     const productDiv = document.querySelector('.products')
+//     productDiv.innerHTML = ' '
+
+//     products.forEach((item, index) => {
+//         productDiv.innerHTML += 
+//         `
+
+        
+//         `
+//     });
+//     }
+// renderproducts()
+
+alert(به سبد خرید اضافه شد)
